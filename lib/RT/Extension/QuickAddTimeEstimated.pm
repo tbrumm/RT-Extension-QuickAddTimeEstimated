7852f9c609f76f4b5ac99c7406a080acafcd27fb
Torsten Brumm <support-rt@my-rt.de>
2013-11-28 14:15:12 +0100

    Create QuickAddTimeEstimated.pm

---
+++ lib/RT/Extension/QuickAddTimeEstimated.pm
@@ -0,0 +1,59 @@
+use strict;
+use warnings;
+package RT::Extension::QuickAddTimeEstimated;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT-Extension-QuickAddTimeEstimated - Quick "add time estimated" entry on the ticket display page
+
+=head1 INSTALLATION
+
+=over
+
+=item C<perl Makefile.PL>
+
+=item C<make>
+
+=item C<make install>
+
+May need root permissions
+
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+
+Add this line:
+
+    Set(@Plugins, qw(RT::Extension::QuickAddTimeEstimated));
+
+or add C<RT::Extension::QuickAddTimeEstimated> to your existing C<@Plugins> line.
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 AUTHOR
+
+Torsten Brumm <tbrumm@mac.com>
+
+=head1 THANKS
+
+Based on the original RT-Extension-QuickAddTimeWorked from
+Thomas Sibley (BPS)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2013 by Best Practical Solutions
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
+
+=cut
+
+1;
